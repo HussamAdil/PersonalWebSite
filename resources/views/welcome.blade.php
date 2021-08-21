381262aa3bba9d7fcd85821bd51a3cf4c289ca18
hussamadil <hussam0683@gmail.com>
2021-08-21 21:53:56 +0200

    change home page color and remove map and about section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -11,6 +11,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.gstatic.com">
 
+        <link rel="icon" href="{{ asset('images/favicon.svg')}}">
+ 
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
          
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -27,14 +29,13 @@
 
          <link href="https://animate.style/animate.min.css" rel="stylesheet">
 
-
       </head>
     <body>
    
     <!-- Start Nabbar -->
     <nav class="navbar navbar-expand-lg p-4   navbar-light bg-light">
         <div class="container " id="pageContent">
-        <a class="navbar-brand logo" href="#" > H</a>
+        <a class="navbar-brand logo" href="#" > Hussam </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <i class="fa fa-navicon"></i>
         </button>
@@ -52,7 +53,7 @@
             </li>
 
             <li class="nav-item">
-              <a class="nav-link nav-link-hover" href="#skills"><i class="mr-1 fa fa-star nav-icon " ></i> Skills</a>
+              <a class="nav-link nav-link-hover" href="#skills"><i class="mr-1 fa fa-star nav-icon " ></i> Technical Skills</a>
             </li>
              
               <li class="nav-item">
@@ -75,29 +76,25 @@
         <div class="container p-3">
         <div class="p-4 ml-4 ">
           <div class="row">
-            <div class="col-sm-9 p-4">
-              <p>Hello Everyone,I'm <span style="
-                color: #fcbf49;
-            ">Hussam Adil </span> 👋</p>
-              <h3 class="" data-in-effect="rollIn"> Freelance Full Stack Web Developer.  </h3>
-              <p class="Im-an-full-stack wow animate__slideInUp mt-3"   data-wow-duration="1s">
+            <div class="col-sm-8 p-4 wow wow animate__slideInDown">
+              
+              <h3 class="" data-in-effect="rollIn">Hello There ,<span class="hussamadil">Hussam Adil</span> </h3>
+              <p class="Im-an-full-stack   mt-3"   data-wow-duration="1s">
                 
-                Web Developer based in Sudan 🇸🇩.
+                I'm a Full Stack Web Developer. based in Sudan 🇸🇩.
 
               I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
               
-              I graduated from <a  target="_blank" class="university-link text-white" href="https://www.siu-sd.com/">Sudan international University</a>.
-              <div class="mt-3  wow animate__slideInUp"   data-wow-duration="1s" >
-                <a href="#contact-me" class="contact-mebtn btn btn-warning"><i class="fa fa-envelope-open-o"> </i> Contact Me</a>
+              I graduated from <a  target="_blank" class="university-link text-dark" href="https://www.siu-sd.com/">Sudan international University</a>.
+              <div class="mt-3 "   data-wow-duration="1s" >
+                <a href="#contact-me" class="contact-mebtn btn btn-warning" style="background:#d2f4f6;border:0"><i class="fa fa-envelope-open-o"> </i> Contact Me</a>
             </div> 
             </p>
             </div>
-            <div class="col-sm-3">
-              <img alt="Hussam Adil "  class="text-center  personal-image   wow animate__fadeInDown" data-wow-delay="1s" src="https://i.stack.imgur.com/zYENw.png?s=328&g=1">
-              <hr>
-              <p   class="mt-2 text-center   d-sm-block wow animate__fadeInLeft" data-wow-delay="1s">
-                it's Me 👆
-              </p>
+            <div class="col-sm-4">
+              <img alt="Hussam Adil "  class="text-center  personal-image   wow animate__fadeInDown" data-wow-delay="1s" src="https://i.ibb.co/yBz2PF6/Project-Drawing-1731238579-1.png">
+            
+             
             </div>
           </div>
            
@@ -105,13 +102,37 @@
         </div>
       </div>
     </div>
+
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#d2f4f6" fill-opacity="1" d="M0,32L288,192L576,224L864,224L1152,320L1440,256L1440,320L1152,320L864,320L576,320L288,320L0,320Z"></path></svg>    <div style="background: #d2f4f6">
+      <div class=" container p-4" id="education">
+       <div class="mb-3  " style="background: #d2f4f6">
+          <span class="h3 mb-3  section-title" style="background:#fff;color:#333"><i class="nav-icon mr-1 fa fa-book" aria-hidden="true"></i> Education</span>
+          <div class="col-sm-8 mt-3 wow animate__slideInDown"  >
+            <p>
+              On October 2017  I graduated from Sudan international university, faculty of  management information system, its combination between IT and management.
+            </p>
+            <p>
+              At college, I learned about business and accounting and marketing, and a lot of cool stuff there.
+            </p>
+            <p>
+              my specialization gives me the ability to think about your project from different points of view, the business one and technical view as well.
+            </p>
+            <span class="cgpa p-1">
+              <i class=" fa fa-check-square"></i> CGPA : 3.48 out of 4.00
+            </span>
+        </div>
+      </div>
+           
+      </div>
+     </div>
+     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#d2f4f6" fill-opacity="1" d="M0,32L288,192L576,224L864,224L1152,320L1440,256L1440,0L1152,0L864,0L576,0L288,0L0,0Z"></path></svg> <!-- end my-skills -->
        <!-- end  intro-->
-        
+    
             <!-- start  work-->
             <div class="work mt-2   p-2" id="work">
               <div class=" p-4 container">
                 <div class="mb-3">
-                  <span class="h3 section-title  "  >Work</span> 
+                  <span class="h3 section-title  "  ><i class="nav-icon mr-1 fa fa-briefcase" aria-hidden="true"></i> Work</span> 
                   <p class=" mt-3 wow animate__fadeInDown" data-wow-delay="1s">Below you can see some projects I've been working on lately.</p>
                   <div class="work-slider text-center mt-4 wow animate__fadeInUp" data-wow-delay="1s">
                     <div>
@@ -148,47 +169,17 @@
 
         <!-- end  work-->
 
-        <!-- end  education-->
-
-         <div class="education mt-4" id="education">
-           <div class=" container p-4">
-            <div class="mb-3 mt-3">
-              <span class="h3 section-title">Education</span>
-          </div>
-              <div class="row" >
-                <div class="col-sm-8 mt-3 wow animate__slideInDown"  >
-                    <p>
-                      On October 2017  I graduated from Sudan international university, faculty of  management information system, its combination between IT and management.
-                    </p>
-                    <p>
-                      At college, I learned about business and accounting and marketing, and a lot of cool stuff there.
-                    </p>
-                    <p>
-                      my specialization gives me the ability to think about your project from different points of view, the business one and technical view as well.
-                    </p>
-                    <span class="cgpa p-1">
-                      <i class=" fa fa-check-square"></i> CGPA : 3.48 out of 4.00
-                    </span>
-                </div>
-                <div class="col-sm-4" >
-                  
-                  <img alt="graduation hat"  class=" img-fluid wow animate__jackInTheBox"  src="https://i.postimg.cc/Dz5THXwX/undraw-Graduation-ktn0-1.png">
-                </div>
-              </div>
-           </div>
-          </div>
-
-           <!-- end  education-->
+    
 
            <!-- start my-skills -->
 
-            <svg xmlns="https://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#130f40" fill-opacity="1" d="M0,96L24,122.7C48,149,96,203,144,202.7C192,203,240,149,288,117.3C336,85,384,75,432,112C480,149,528,235,576,234.7C624,235,672,149,720,133.3C768,117,816,171,864,197.3C912,224,960,224,1008,208C1056,192,1104,160,1152,170.7C1200,181,1248,235,1296,245.3C1344,256,1392,224,1416,208L1440,192L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z"></path></svg>
+            <svg xmlns="https://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#d2f4f6" fill-opacity="1" d="M0,96L24,122.7C48,149,96,203,144,202.7C192,203,240,149,288,117.3C336,85,384,75,432,112C480,149,528,235,576,234.7C624,235,672,149,720,133.3C768,117,816,171,864,197.3C912,224,960,224,1008,208C1056,192,1104,160,1152,170.7C1200,181,1248,235,1296,245.3C1344,256,1392,224,1416,208L1440,192L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z"></path></svg>
 
             <div class="my-skills" id="skills">
               <div class=" container p-4">
                <div class="mb-3  ">
-                  <span class="h3 mb-3 mt-3 section-title">My Skills</span>
-                   <p class="mt-3 text-white">Here are a few technologies I've been working with recently:</p>
+                  <span class="h3 mb-3 mt-3 section-title" style="background: #fff">Technical Skills</span>
+                   <p class="mt-3 ">Here are a few technologies I've been working with recently:</p>
               </div>
                   <div class="row my-skill-list mt-4">    
                          <p class=" mb-4 col-sm-4 wow animate__slideInDown">PHP</p>
@@ -201,7 +192,7 @@
                     </div>
               </div>
              </div>
-             <svg xmlns="https://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#130f40" fill-opacity="1" d="M0,64L30,106.7C60,149,120,235,180,240C240,245,300,171,360,138.7C420,107,480,117,540,117.3C600,117,660,107,720,90.7C780,75,840,53,900,74.7C960,96,1020,160,1080,186.7C1140,213,1200,203,1260,176C1320,149,1380,107,1410,85.3L1440,64L1440,0L1410,0C1380,0,1320,0,1260,0C1200,0,1140,0,1080,0C1020,0,960,0,900,0C840,0,780,0,720,0C660,0,600,0,540,0C480,0,420,0,360,0C300,0,240,0,180,0C120,0,60,0,30,0L0,0Z"></path></svg>             <!-- end my-skills -->
+             <svg xmlns="https://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#d2f4f6" fill-opacity="1" d="M0,64L30,106.7C60,149,120,235,180,240C240,245,300,171,360,138.7C420,107,480,117,540,117.3C600,117,660,107,720,90.7C780,75,840,53,900,74.7C960,96,1020,160,1080,186.7C1140,213,1200,203,1260,176C1320,149,1380,107,1410,85.3L1440,64L1440,0L1410,0C1380,0,1320,0,1260,0C1200,0,1140,0,1080,0C1020,0,960,0,900,0C840,0,780,0,720,0C660,0,600,0,540,0C480,0,420,0,360,0C300,0,240,0,180,0C120,0,60,0,30,0L0,0Z"></path></svg>             <!-- end my-skills -->
    
          <!-- end my-skills -->
 
@@ -239,14 +230,7 @@
         </div>
           
       </div>
-          <div class=" mt-4 ">
-            <div class="mb-3 mt-3">
-              <span class="h3 section-title">My Location</span>
-          </div>
-            <div >
-            <iframe class="col-sm-12 google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2713293.93630849!2d30.497535229524164!3d14.822685900281384!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x15d91cc41072b195%3A0xd2bf70462a612b37!2sSudan!5e0!3m2!1sen!2s!4v1606239934774!5m2!1sen!2s"   allowfullscreen="" aria-hidden="false"   tabindex="0"></iframe>         
-           </div>
-          </div>
+           
       </div>
      </div>
      </div>
@@ -257,9 +241,9 @@
 
       <footer>
         <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-          <path fill="#130f40" fill-opacity="1" d="M0,64L26.7,106.7C53.3,149,107,235,160,272C213.3,309,267,299,320,266.7C373.3,235,427,181,480,160C533.3,139,587,149,640,149.3C693.3,149,747,139,800,144C853.3,149,907,171,960,186.7C1013.3,203,1067,213,1120,213.3C1173.3,213,1227,203,1280,181.3C1333.3,160,1387,128,1413,112L1440,96L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"></path>
+          <path fill="#d2f4f6" fill-opacity="1" d="M0,64L26.7,106.7C53.3,149,107,235,160,272C213.3,309,267,299,320,266.7C373.3,235,427,181,480,160C533.3,139,587,149,640,149.3C693.3,149,747,139,800,144C853.3,149,907,171,960,186.7C1013.3,203,1067,213,1120,213.3C1173.3,213,1227,203,1280,181.3C1333.3,160,1387,128,1413,112L1440,96L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"></path>
         </svg>      
-        <div class="my-footer text-center text-white"  id='footerlink' >
+        <div class="my-footer text-center"  id='footerlink' >
           <span style="" >Designed and built with lots of love. <i class="fa fa-heart"></i></span>
           
           <br>
